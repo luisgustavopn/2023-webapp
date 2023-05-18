1e8469d7379e6c98b81328fd0be5a599edc07371
helio-filho99 <helioosilvafilho@gmail.com>
2023-05-18 10:27:29 -0300

    criando um botão

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Botao = (props: any) => {
+
+  return(
+    <div>
+      <button>Meu primeiro botão</button>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +26,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Botao></Botao>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
