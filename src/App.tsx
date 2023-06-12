b8cbd6c50129708f28b9fce87e6a9bc994f8c4e6
Luis Gustavo Pereira Nunes <g.luis@academico.ifrn.edu.br>
2023-06-12 18:48:16 -0300

    aula de elevação de estado 12/06

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,105 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+const Entrada = (props: any) => {
+
+  return (
+  <div>
+    <label>
+      nova tarefa
+      <input type='text' value={props.valor} />
+      <button>adicionar</button>
+    </label>
+  </div>
+  )
+}
+
+const Tarefas = (props: any) => {
+  return (
+    <div className = 'card'>
+      <ul>
+        {props.valor.map( (item : string, index : number) => <li key={index}>{item}
+        </li> )}
+      </ul>
+    </div>
+  )
+}
+
 
 const Botao = (props: any) => {
 
+  const [contador , setContador] = useState(0);
+
+  const handleMudar = () => {
+    setContador(contador + 1);
+    console.log('Olá boa noite Zé da Manga')
+  }
+
   return(
     <div>
-      <button>Meu primeiro botão</button>
+      <button onClick={handleMudar}>
+        O contador está em {contador}
+      </button>
+    </div>
+  )
+}
+
+const MeuBotao =  (props:any) => {
+
+  return(
+    <div>
+      <button onClick={props.funcao}>
+        {props.titulo}
+      </button>
     </div>
   )
 }
 
 function App() {
+  const [tarefa, setTarefa] = useState('digite nova tarefa')
+  const [tarefas, setTarefas] = useState(['tarefa um','tarefa 2'])
   const [count, setCount] = useState(0)
+  const DarBomDia = () => {
+    console.log("Olá bom dia")
+  }
+  const DarBoaTarde = () => {
+    console.log("Olá boa tarde")
+  }
+  const DarBoaNoite = () => {
+    console.log("Olá boa noite")
+  }
 
   return (
+    
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Botao></Botao>
+      <Entrada valor={tarefa}/>
+      <Tarefas valor={tarefas}/>
+      <MeuBotao 
+        titulo = {'Zé da Manga'}
+        funcao = {DarBomDia}
+      >
+      </MeuBotao>
+
+      <MeuBotao 
+        titulo = {'Zé da Manga'}
+        funcao = {DarBoaTarde}
+      >
+      </MeuBotao>
+
+      <MeuBotao 
+        titulo = {'Zé da Manga'}
+        funcao = {DarBoaNoite}
+      >
+      </MeuBotao>
+      
+      <Botao/>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
